2fcbaa5d77d73c282941c2d46e99e7e538423c40
212411300 <racheliseitz1@gmail.com>
2026-01-03 22:32:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,33 +3,58 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from skimage.filters import median
 from skimage.morphology import disk
+from scipy.signal import convolve2d # Added for edge_detection
 
-from image_utils import load_image 
+from image_utils import load_image # Only import load_image, edge_detection will be defined here
+
+# Redefine edge_detection to ensure correct parameter usage
+def edge_detection(image_array):
+    # If the image_array is already grayscale (2D), use it directly.
+    # Otherwise, convert 3-channel image to grayscale.
+    if image_array.ndim == 3:
+        gray_image = np.mean(image_array, axis=2)
+    else:
+        gray_image = image_array
+
+    kernelY = np.array([[ 1,  0, -1],
+                        [ 2,  0, -2],
+                        [ 1,  0, -1]])
 
 
-path = '/content/sample_data/IMG-20260103-WA0013.jpg'
-original_img = load_image(path)
+    kernelX = np.array([[-1, -2, -1],
+                        [ 0,  0,  0],
+                        [ 1,  2,  1]])
 
-gray_img = np.mean(original_img, axis=2)
+    edgeY = convolve2d(gray_image, kernelY, mode='same', boundary='fill', fillvalue=0)
+    edgeX = convolve2d(gray_image, kernelX, mode='same', boundary='fill', fillvalue=0)
 
-clean_image = median(gray_img, disk(3))
+    edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
 
-edgeMAG = edge_detection(original_img)
+    return edgeMAG
 
+
+path = '/content/IMG-20260103-WA0013.jpg'
+original_np = load_image(path)
+
+gray_image = np.mean(original_np, axis=2).astype(np.uint8)
+clean_image = median(gray_image, disk(3))
+
+edgeMAG = edge_detection(clean_image)
 
 plt.figure(figsize=(10, 4))
-plt.hist(edgeMAG.ravel(), bins=256, range=(0, 100))
-plt.title("Histogram of Edge Intensities")
-plt.show()
+plt.subplot(1, 2, 1)
+plt.hist(edgeMAG.ravel(), bins=256, color='black')
+plt.title("Histogram of edgeMAG")
+plt.xlabel("Pixel Intensity")
+plt.ylabel("Frequency")
 
-threshold = 40
-edge_binary = edgeMAG > threshold
+threshold_value = 50
+edge_binary = edgeMAG > threshold_value
 
+plt.subplot(1, 2, 2)
 plt.imshow(edge_binary, cmap='gray')
-plt.title(f"Edges with Threshold = {threshold}")
-plt.axis('off')
+plt.title("Binary Edge Map")
 plt.show()
 
 edge_image = Image.fromarray((edge_binary * 255).astype(np.uint8))
 edge_image.save('my_edges.png')
-
